994a1fe6ba74c968c16fac1f7d3f7f829f5232c4
vladphytonmachine <vladdylyver@gmail.com>
2024-08-18 17:32:30 +0300

    Add files via upload

---
+++ lesson 3.py
@@ -0,0 +1,5 @@
+number_of_completed_homework_assignments=12
+number_of_hours_spent=1.5
+сourse_name="Phyton"
+time_for_one_task=((number_of_hours_spent)/ (number_of_completed_homework_assignments))
+print ('Курс:',сourse_name,"," 'всего задач',number_of_completed_homework_assignments,","'затрачено часов' ,number_of_hours_spent, "," 'среднее время выполнения ', time_for_one_task)
